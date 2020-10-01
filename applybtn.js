7ff4659b5c77cc323faea099c6c965c929396fd6
Michael-Bissinger <Michael.Bissinger@inb.de>
2020-10-01 15:23:31 +0200

    onclick working for apply

---
+++ applybtn.js
@@ -0,0 +1,8 @@
+
+        window.onload = function(){
+          document.getElementById("button_apply_1").addEventListener("click", displayDate);
+      
+          function displayDate() {
+            alert("hi");
+          }
+        }
