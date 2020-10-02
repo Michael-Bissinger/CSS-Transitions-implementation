08fdee36cb62727096ed4243bc7bb2945dcd4437
Michael-Bissinger <Michael.Bissinger@inb.de>
2020-10-02 12:07:05 +0200

    Put navigation and style for SPA in external script and css

---
+++ spa_navigation.js
@@ -0,0 +1,24 @@
+      let pages = ["transition1", "transition2", "transition3", "welcome"];
+      
+      function changeNavigation(page) {
+        for (let p of pages) {
+				    let el = document.getElementById(p)
+				  if (p === page)
+					  el.classList.remove("hidden")
+				  else
+					  el.classList.add("hidden")
+			}
+      }
+
+      window.addEventListener("hashchange", event => {
+      let p = window.location.hash.replace("#","")
+/*        if(p === "") {
+        p = "welcome"
+      } */ /*gelöst durch eventlistener on load */
+        
+      changeNavigation(window.location.hash.replace("#",""));
+    })
+    
+    window.addEventListener("load", event  => {
+      changeNavigation("welcome"); /* Was passiert bei erstem Laden, wenn im Hash noch nix steht */
+    })
