0b3e3f78ffe96a2b9672f5736d91ad8c117246bb
Michael-Bissinger <Michael.Bissinger@inb.de>
2020-10-05 13:44:06 +0200

    Cleanup code from applybtn

--- applybtn.js
+++ applybtn.js
@@ -15,24 +15,22 @@
           function applyStyle(trans_number) {
             alert("Button " + trans_number + " clicked!")
 
-      
-
             /* --- Read settings --- */
-              /* Property */ /* credits to https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript */
+              /* GET Property */ /* credits to https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript */
               var option_prop_trans = document.getElementById("property_trans1");
               var property_user = option_prop_trans.options[option_prop_trans.selectedIndex].text;
               alert("PROPERTY: " + property_user);    
 
-              /* Duration */
+              /* GET Duration */
               var duration_user = document.getElementById("duration_trans1").value;
               alert("DURATION: " + duration_user);  
               
-              /* Timing */ /* credits to https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript */
+              /* GET Timing */ /* credits to https://stackoverflow.com/questions/1085801/get-selected-value-in-dropdown-list-using-javascript */
               var option_time_trans = document.getElementById("timing_trans1");
               var timing_user = option_time_trans.options[option_time_trans.selectedIndex].text;
               alert("TIMING: " + timing_user);
               
-              /* Delay */
+              /* GET Delay */
               var delay_user = document.getElementById("delay_trans1").value;
               alert("DELAY: " + delay_user);
               
@@ -42,33 +40,12 @@
               alert(transitionelement);
               console.log(transitionelement);              
 
-
-              /*transitionelement.setAttribute("style", "transition-duration: 8000ms; transition-property: all; transition-timing-function: ease-in-out; transition-delay: 100ms;");
-              transitionelement.style.color = "green";
-              */
-              document.getElementById("button_apply_" + trans_number).setAttribute("style", "transition-duration: 8000ms; transition-property: all; transition-timing-function: ease-in-out; transition-delay: 100ms;");
-              document.getElementById("button_apply_" + trans_number).style.color = "green";
-
-              /*"transition-duration: 1000ms; transition-property: all; transition-timing-function: ease-in-out; transition-delay: 100ms" */
-
-
+              /* --- Adjust style --- */
               /*transitionelement.setAttribute("style", "transition-duration: 8000ms; transition-property: all; transition-timing-function: ease-in-out; transition-delay: 100ms;");*/
               transitionelement.setAttribute("style", "transition-duration: " + duration_user + "ms; transition-property: " + property_user + "; transition-timing-function: " + timing_user + "; transition-delay: " + delay_user + "ms;");
-
-              /* Property */
-              /*transitionelement.style.transition-property = "all";
-              
-
-              /* Duration */
-              /*transitionelement.style.transition-duration = "8000ms";
-              
-              /* Timing */
-              
-              /* Delay */
-              
               
               /* Write message */
-              alert("Style angewendet!");
+              alert("Style auf Transition " + trans_number + " angewendet!");
             
           }
         }
