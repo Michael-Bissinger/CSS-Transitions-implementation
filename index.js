9b8eb9f754dfb19b92e06ff190f6c66312e83c0b
Michael-Bissinger <Michael.Bissinger@inb.de>
2020-10-11 00:00:02 +0200

    Add index.js and rearrange files

---
+++ index.js
@@ -0,0 +1,19 @@
+/* applikation for transitions */
+
+const express = require('express')
+const userRouter = require('./user-router')
+const dataRouter = require('./data-router')
+const bankRouter = require('./transition-router')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.use('/', express.static('www'))
+app.use('/lib', express.static('node_modules'))
+
+app.use('/users/', userRouter)
+app.use('/data', dataRouter)
+app.use('/konten', bankRouter)
+
+app.listen(port, () => console.log(`Open http://localhost:${port}`))
--- transition-router.js
+++ transition-router.js
@@ -85,3 +85,5 @@ module.exports = router
 
 konto = transition
 Konto = Transitionsetup
+
+=> Wurde umgebaut zu einer REST-Anwendung. Wir schicken kein HTML mehr, sondern nur noch JSON
