4c795e4a41f3f37fcf1d0764a0575489503d0cfd
Michael-Bissinger <Michael.Bissinger@inb.de>
2020-10-28 15:21:33 +0100

    Refactor data-router

--- data-router.js
+++ data-router.js
@@ -1,33 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
-function getRandomName() {
-	const names = ["Peter", "Petra", "Kay-Sölve", "Max"]
-	const n = Math.floor(Math.random() * (names.length - 1))
-	return names[n]
-}
-
 /* https://expressjs.com/de/guide/routing.html */
 
 router.get('/', (req, res) => {
 	res.status(200)
 		.type("json")
 		.send({
-			"property_trans1": getRandomName(),
-			"property_trans2": getRandomName(),
-			"property_trans3": getRandomName(),
+			"property_trans1": string,
+			"property_trans2": string,
+			"property_trans3": string,
 
-			"duration_trans1": Math.floor(Math.random() * 1000),
-			"duration_trans2": Math.floor(Math.random() * 1000),
-			"duration_trans3": Math.floor(Math.random() * 1000),
+			"duration_trans1": number,
+			"duration_trans2": number,
+			"duration_trans3": number,
 
-			"timing_trans1": getRandomName(),
-			"timing_trans2": getRandomName(),
-			"timing_trans3": getRandomName(),
+			"timing_trans1": string,
+			"timing_trans2": string,
+			"timing_trans3": string,
 
-			"delay_trans1": Math.floor(Math.random() * 1000),
-			"delay_trans2": Math.floor(Math.random() * 1000),
-			"delay_trans3": Math.floor(Math.random() * 1000)
+			"delay_trans1": number,
+			"delay_trans2": number,
+			"delay_trans3": number
 
 		})
 		.end();
